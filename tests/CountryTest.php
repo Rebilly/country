21452d034840545dab250dcceb6c7f431e7cfa77
Alexander Shkarpetin <ashkarpetin@gmail.com>
2018-12-11 01:38:37 +0600

    Add more tests

--- tests/CountryTest.php
+++ tests/CountryTest.php
@@ -9,43 +9,22 @@ class CountryTest extends TestCase
 {
     /**
      * @test
+     * @dataProvider provideInvalidData
      */
-    public function invalidConstructionThrowsException(): void
+    public function invalidConstructionThrowsException(array $data, array $expected): void
     {
-        $isoAlpha2 = 'ZZ';
-        $isoAlpha3 = 'XYZ';
-        $isoNumeric = 9999;
-        $commonName = 'Common Name';
-        $officialName = 'Official Name';
-        $continent = 'Asia';
-        $topLevelDomain = 'tld';
-        $longDistancePrefix = 99;
-        $currencyIsoAlpha = 'XYD';
-
-        $this->expectException(InvalidArgumentException::class);
+        $this->expectException($expected[0]);
+        $this->expectExceptionMessage($expected[1]);
         new Country(
-            $isoAlpha3,
-            $isoAlpha3,
-            $isoNumeric,
-            $commonName,
-            $officialName,
-            $continent,
-            $longDistancePrefix,
-            $topLevelDomain,
-            $currencyIsoAlpha
-        );
-
-        $this->expectException(InvalidArgumentException::class);
-        new Country(
-            $isoAlpha2,
-            $isoAlpha2,
-            $isoNumeric,
-            $commonName,
-            $officialName,
-            $continent,
-            $longDistancePrefix,
-            $topLevelDomain,
-            $currencyIsoAlpha
+            $data['isoAlpha2'],
+            $data['isoAlpha3'],
+            9999,
+            'Common Name',
+            'Official Name',
+            'Asia',
+            99,
+            'tld',
+            'XYD'
         );
     }
 
@@ -77,6 +56,11 @@ class CountryTest extends TestCase
         );
         $this->assertInstanceOf(Country::class, $country);
         $this->assertSame($isoAlpha2, $country->getIsoAlpha2());
+        $this->assertSame($isoAlpha3, $country->getIsoAlpha3());
+        $this->assertSame($isoAlpha2, (string) $country);
+        $this->assertSame($longDistancePrefix, $country->getLongDistancePrefix());
+        $this->assertSame($isoNumeric, $country->getIsoNumeric());
+        $this->assertSame($currencyIsoAlpha, $country->getCurrencyIsoAlphaCode());
     }
 
     /**
@@ -119,5 +103,19 @@ class CountryTest extends TestCase
         );
 
         $this->assertTrue($country->equals($canada));
+    }
+
+    public function provideInvalidData(): array
+    {
+        return [
+            [
+                ['isoAlpha2' => 'USA', 'isoAlpha3' => 'USA'],
+                [InvalidArgumentException::class, 'IsoAlpha2 must be a 2 character string']
+            ],
+            [
+                ['isoAlpha2' => 'US', 'isoAlpha3'  => 'US'],
+                [InvalidArgumentException::class, 'IsoAlpha3 must be a 3 character string']
+            ],
+        ];
     }
 }
